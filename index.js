c2dd32bcff5b1296f6b02fa4c0a3f27f28860ffd
david <davidealvarez1@gmail.com>
2020-05-22 08:06:18 -0700

    Hello WOrld!

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 3300;
+server.get('/', (req,res)=>{
+  res.json("Hello World!")
+})
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
